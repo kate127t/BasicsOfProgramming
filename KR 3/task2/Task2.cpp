5464fdd649adf6544a4955a210de33f57498e212
kate127t <79634757+kate127t@users.noreply.github.com>
2021-05-13 13:13:42 +0300

    Update Task2.cpp

--- KR 3/task2/Task2.cpp
+++ KR 3/task2/Task2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<string.h>
 #include<fstream>
 using namespace std;
 #define SIZE 30
@@ -24,6 +25,7 @@ char** read_from_file(char* filename) {
 
 char** only_prog(char** prog, int size1, char** history, int size2) {
 	bool is_only_prog = true;
+	
 	char** only_prog = new char* [size1];
 	int k = 0;
 	for (int i = 0; i < size1; i++) {
@@ -35,7 +37,39 @@ char** only_prog(char** prog, int size1, char** history, int size2) {
 		if (is_only_prog == true) {
 			only_prog[k++] = prog[i];
 		}
-
 	}
+	
+	char** result = new char* [k-1];
 
+	for (int j = 0; j < k - 1; j++) {
+		strcpy_s(result[j], SIZE, only_prog[j]);
+		cout << result[j];
+	}
+	delete[] only_prog;
+	return result;
+}
+
+void main() {
+	int size1,size2;
+
+	ifstream fin("in1.txt");
+	fin >> size1;
+	fin.close();
+
+	ifstream fin("in2.txt");
+	fin >> size2;
+	fin.close();
+
+	const char* name1 = "in1.txt";
+	char* filename1 = new char[100];
+	strcpy_s(filename1, 100, name1);
+
+	const char* name2 = "in2.txt";
+	char* filename2 = new char[100];
+	strcpy_s(filename2, 100, name2);
+
+	char** prog = read_from_file(filename1);
+	char** history = read_from_file(filename2);
+
+	char** only_programming = only_prog(prog, size1, history, size2);
 }
