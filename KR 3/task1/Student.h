9a0b6f570996ce15485adb69ba8430e5683b988b
kate127t <79634757+kate127t@users.noreply.github.com>
2021-05-13 12:57:36 +0300

    Update Student.h

--- KR 3/task1/Student.h
+++ KR 3/task1/Student.h
@@ -1,1 +1,14 @@
 #pragma once
+#include<iostream>
+using namespace std;
+#define SIZE 30
+
+struct Student {
+	char* LastName;
+	char* FirstName;
+	int Year;
+	char* Group;
+	char* StudNum;
+	Student();
+	~Student();
+};
